59539c175c32af4e27cd05b8f97fd7c791c54969
Cristian Suárez <cristiansuarezg7@gmail.com>
2021-09-11 16:24:04 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4ff05e835a89a6247379df81affe3add'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d228b69-8d08-460e-a1be-ffc1c9f7affe'
+};
